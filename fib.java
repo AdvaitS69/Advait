83cf4d5d891d9a8204985bbb335fe1065eb23504
Advait <134807249+AdvaitS69@users.noreply.github.com>
2024-04-08 12:58:41 +0530

    Update fib.java

--- fib.java
+++ fib.java
@@ -1,4 +1,4 @@
-public class Fibonacci {
+public class fib {
     public static void main(String[] args) {
         int n = 20; // Number of Fibonacci numbers to print
         int[] fibonacci = new int[n];
