a7462e39fc96ff3c8c421c5945c4a319ecc3ec21
Advait <134807249+AdvaitS69@users.noreply.github.com>
2024-04-08 13:00:17 +0530

    Update fib.java

--- fib.java
+++ fib.java
@@ -1,7 +1,15 @@
 public class fib {
-    public static void main(String[] args) {
-        int n = 20; // Number of Fibonacci numbers to print
-        int[] fibonacci = new int[n];
-        
-        // First two Fibonacci numbers
-        fibonacci[0] = 0
+
+  public static void main(String[] args) {
+    int n1 = 0, n2 = 1, count = 20;
+
+    System.out.print(n1 + " " + n2);
+
+    for (int i = 2; i < count; ++i) {
+      int n3 = n1 + n2;
+      System.out.print(" " + n3);
+      n1 = n2;
+      n2 = n3;
+    }
+  }
+}
