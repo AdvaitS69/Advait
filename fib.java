c2d3224c6d3c1ce22aa7e92122ac9fdbdde10680
Advait <134807249+AdvaitS69@users.noreply.github.com>
2024-04-08 12:55:02 +0530

    Add files via upload

---
+++ fib.java
@@ -0,0 +1,7 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 20; // Number of Fibonacci numbers to print
+        int[] fibonacci = new int[n];
+        
+        // First two Fibonacci numbers
+        fibonacci[0] = 0
